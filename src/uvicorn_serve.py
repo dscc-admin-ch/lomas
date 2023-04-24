d96b5a590e07b25bcf0a2209c37be5de4c052c0a
Damien Aymon <damienaymon@gmail.com>
2023-04-24 11:15:07 +0200

    backup

--- src/uvicorn_serve.py
+++ src/uvicorn_serve.py
@@ -4,4 +4,4 @@ from app import app
 
 if __name__ == "__main__":
     os.chdir("/code/")
-    uvicorn.run("app:app", host="0.0.0.0", port=80, log_level="info", workers=5, reload=True)
+    uvicorn.run("app_minimal:app", host="0.0.0.0", port=80, log_level="info", workers=5, reload=True)

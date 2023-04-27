24980d36f108ac0c376c474339cc3c74566aedec
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2023-04-27 15:20:53 +0200

    adding pyproject.toml, forcing everything with black and fixing ALL the flake8 issues

--- src/example_inputs.py
+++ src/example_inputs.py
@@ -1,69 +1,69 @@
 example_diffprivlib = {
-  "module": "diffprivlib",
-  "version": "0.6.0",
-  "pipeline": [
-    {
-      "type": "_dpl_type:StandardScaler",
-      "name": "scaler",
-      "params": {
-        "with_mean": True,
-        "with_std": True,
-        "copy": True,
-        "epsilon": 1,
-        "bounds": {
-          "_tuple": True,
-          "_items": [
-            [
-              17,
-              1,
-              0,
-              0,
-              1
-            ],
-            [
-              90,
-              160,
-              10000,
-              4356,
-              99
-            ]
-          ]
+    "module": "diffprivlib",
+    "version": "0.6.0",
+    "pipeline": [
+        {
+            "type": "_dpl_type:StandardScaler",
+            "name": "scaler",
+            "params": {
+                "with_mean": True,
+                "with_std": True,
+                "copy": True,
+                "epsilon": 1,
+                "bounds": {
+                    "_tuple": True,
+                    "_items": [
+                        [
+                            17,
+                            1,
+                            0,
+                            0,
+                            1,
+                        ],
+                        [
+                            90,
+                            160,
+                            10000,
+                            4356,
+                            99,
+                        ],
+                    ],
+                },
+                "accountant": "_dpl_instance:BudgetAccountant",
+            },
         },
-        "accountant": "_dpl_instance:BudgetAccountant"
-      }
-    },
-    {
-      "type": "_dpl_type:PCA",
-      "name": "pca",
-      "params": {
-        "n_components": 2,
-        "copy": True,
-        "whiten": False,
-        "random_state": None,
-        "centered": True,
-        "epsilon": 1,
-        "data_norm": 5,
-        "bounds": None,
-        "accountant": "_dpl_instance:BudgetAccountant"
-      }
-    },
-    {
-      "type": "_dpl_type:LogisticRegression",
-      "name": "lr",
-      "params": {
-        "tol": 0.0001,
-        "C": 1,
-        "fit_intercept": True,
-        "max_iter": 100,
-        "verbose": 0,
-        "warm_start": False,
-        "n_jobs": None,
-        "epsilon": 1,
-        "data_norm": 5,
-        "accountant": "_dpl_instance:BudgetAccountant"
-      }
-    }
-  ]
+        {
+            "type": "_dpl_type:PCA",
+            "name": "pca",
+            "params": {
+                "n_components": 2,
+                "copy": True,
+                "whiten": False,
+                "random_state": None,
+                "centered": True,
+                "epsilon": 1,
+                "data_norm": 5,
+                "bounds": None,
+                "accountant": "_dpl_instance:BudgetAccountant",
+            },
+        },
+        {
+            "type": "_dpl_type:LogisticRegression",
+            "name": "lr",
+            "params": {
+                "tol": 0.0001,
+                "C": 1,
+                "fit_intercept": True,
+                "max_iter": 100,
+                "verbose": 0,
+                "warm_start": False,
+                "n_jobs": None,
+                "epsilon": 1,
+                "data_norm": 5,
+                "accountant": "_dpl_instance:BudgetAccountant",
+            },
+        },
+    ],
 }
 
 
@@ -79,14 +79,23 @@ example_opendp = {
                 "module": "trans",
                 "type": "Transformation",
                 "args": [],
-                "kwargs": {"key": "income", "TOA": "py_type:str"},
+                "kwargs": {
+                    "key": "income",
+                    "TOA": "py_type:str",
+                },
             },
             {
                 "func": "make_split_dataframe",
                 "module": "trans",
                 "type": "Transformation",
                 "args": [],
-                "kwargs": {"separator": ",", "col_names": ["hello", "world"]},
+                "kwargs": {
+                    "separator": ",",
+                    "col_names": [
+                        "hello",
+                        "world",
+                    ],
+                },
             },
         ],
         "kwargs": {},
@@ -94,13 +103,12 @@ example_opendp = {
 }
 
 example_smartnoise_sql = {
-  "query_str": "SELECT COUNT(col2) FROM comp.comp GROUP BY col1",
-  "epsilon": 10,
-  "delta": 0
+    "query_str": "SELECT COUNT(col2) FROM comp.comp GROUP BY col1",
+    "epsilon": 10,
+    "delta": 0,
 }
 
 example_smartnoise_synth = {
-  "model": "MWEM",
-  "epsilon": 1,
-
+    "model": "MWEM",
+    "epsilon": 1,
 }
--- src/input_models.py
+++ src/input_models.py
@@ -1,17 +1,35 @@
 import globals
-from typing import List, Any
-from pydantic import BaseModel, validator, Field
-from diffprivlib_json.diffprivl import diffpriv_map
-from smartnoise_json.synth import synth_map
+from typing import List
+from pydantic import (
+    BaseModel,
+    validator,
+    Field,
+)
+from smartnoise_json.synth import (
+    synth_map,
+)
 import json
-import numpy as np
+
 
 class BasicModel(BaseModel):
     def toJSON(self):
-        return json.loads(json.dumps(self, default=lambda o: o.__str__() if type(o).__name__ == "datetime" else o.__dict__))
+        return json.loads(
+            json.dumps(
+                self,
+                default=lambda o: o.__str__()
+                if type(o).__name__ == "datetime"
+                else o.__dict__,
+            )
+        )
 
     def toJSONStr(self):
-        return json.dumps(self, default=lambda o: o.__str__() if type(o).__name__ == "datetime" else o.__dict__)
+        return json.dumps(
+            self,
+            default=lambda o: o.__str__()
+            if type(o).__name__ == "datetime"
+            else o.__dict__,
+        )
+
 
 class OpenDPBase(BasicModel):
     function: str
@@ -26,7 +44,7 @@ class OpenDPAST(BasicModel):
     args: dict = {}
     kwargs: dict = {}
 
-    
+
 class OpenDPInp(BasicModel):
     # from_function: OpenDPBase
     # to_function: OpenDPBase
@@ -34,62 +52,93 @@ class OpenDPInp(BasicModel):
     ast: OpenDPAST
 
 
-
 class DiffPrivLibModel(BasicModel):
     name: str
-    type: str 
-    # epsilon: float = Field(..., gt=0, le=10) #Could take single epsilon value and divide by len of pipeline to set epsilon for individual pipeline
+    type: str
+
+    # Could take single epsilon value and divide by len of pipeline
+    # to set epsilon for individual pipeline
+    # epsilon: float = Field(..., gt=0, le=10)
+
     params: dict
-    
+
     # @validator('type')
     # def validate_model(cls, type):
     #     if type not in diffpriv_map.keys():
-    #         raise ValueError(f"'{type}' is not one of {list(diffpriv_map.keys())}.")
+    #         raise ValueError(f"'{type}' is not \
+    #                one of {list(diffpriv_map.keys())}.")
     #     return type
 
 
-
 class DiffPrivLibInp(BasicModel):
-    module: str 
+    module: str
     pipeline: List[DiffPrivLibModel]
     version: str
     y_column: str
 
-    @validator('y_column')
+    @validator("y_column")
     def valid_Y(cls, y_column):
         if y_column not in globals.TRAIN_Y.columns:
-            raise ValueError(f"Provided y value '{y_column}' is not available. Please select one of {list(globals.TRAIN_Y.columns)}.")
+            raise ValueError(
+                f"Provided y value '{y_column}' is not available. \
+                    Please select one of {list(globals.TRAIN_Y.columns)}."
+            )
         return y_column
-    @validator('module')
+
+    @validator("module")
     def valid_module(cls, module):
         if module != "diffprivlib":
             raise ValueError(f"'{module}' is not diffprivlib.")
         return module
 
+
 class SNSQLInp(BasicModel):
     query_str: str
-    epsilon: float = Field(..., gt=0, le=globals.EPSILON_LIMIT)
-    delta: float = Field(..., gt=0, le=globals.DELTA_LIMIT)
+    epsilon: float = Field(
+        ...,
+        gt=0,
+        le=globals.EPSILON_LIMIT,
+    )
+    delta: float = Field(
+        ...,
+        gt=0,
+        le=globals.DELTA_LIMIT,
+    )
 
- 
+
 class SNSynthInp(BasicModel):
     model: str
-    epsilon: float = Field(..., gt=0, le=globals.EPSILON_LIMIT)
+    epsilon: float = Field(
+        ...,
+        gt=0,
+        le=globals.EPSILON_LIMIT,
+    )
     delta: float = 0
     select_cols: List = []
     params: dict = {}
     mul_matrix: List = []
 
-    @validator('model')
+    @validator("model")
     def valid_model(cls, model):
         if model not in synth_map.keys():
-            raise ValueError(f"'{model}' is not one of {list(synth_map.keys())}.")
+            raise ValueError(
+                f"'{model}' is not one of {list(synth_map.keys())}."
+            )
         return model
 
-    @validator('delta')
+    @validator("delta")
     def valid_delta(cls, delta, values):
         if values.get("model", "na") not in synth_map.keys():
-            raise ValueError(f"{values.get('model')} is not one of {list(synth_map.keys())}.")
-        if values["model"] != "MWEM" and (not delta > 0 or delta > globals.DELTA_LIMIT):
-            raise ValueError(f"For models other than MWEM delta value should be greater than 0 and less than or equal to {globals.DELTA_LIMIT}. User provided: {str(delta)}")
+            raise ValueError(
+                f"{values.get('model')} is not \
+                    one of {list(synth_map.keys())}."
+            )
+        if values["model"] != "MWEM" and (
+            not delta > 0 or delta > globals.DELTA_LIMIT
+        ):
+            raise ValueError(
+                f"For models other than MWEM delta value should be greater \
+                    than 0 and less than or equal to {globals.DELTA_LIMIT}. \
+                        User provided: {str(delta)}"
+            )
         return delta

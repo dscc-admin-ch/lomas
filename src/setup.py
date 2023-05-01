e2bfb9ebf86da3ec55aa5582a22b5e2cbb95b6f9
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2023-05-01 09:27:24 +0200

    forgot to add some files to last commits..

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sdd_poc_server_package",
+    description="SDD POC Server Package.",
+    version="1.0.0",
+    author="DSCC",
+    author_email="DSCC@bfs.admin.ch",
+    packages=find_packages(),
+)

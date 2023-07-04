cc2ce3c0efae65458a0fc1975fdb6bc52d8d4292
pauline.maury-laribiere <pauline.maury-laribiere@bfs.admin.ch>
2023-07-04 14:25:14 +0000

    getting mongo credentials from vault

--- src/database/utils.py
+++ src/database/utils.py
@@ -1,7 +1,13 @@
 import os
 import hvac
 
-from utils.constants import CONF_DB_TYPE_MONGODB, CONF_DB_TYPE_YAML, DATABASE_NAME, MONGODB_PORT
+from utils.constants import (
+    CONF_DB_TYPE_MONGODB,
+    CONF_DB_TYPE_YAML,
+    DATABASE_NAME,
+    MONGODB_PORT,
+    VAULT_NAME
+)
 from database.database import Database
 from database.mongodb_database import MongoDB_Database
 from database.yaml_database import YamlDatabase
@@ -21,15 +27,7 @@ def database_factory(config: DBConfig) -> Database:
         return YamlDatabase(yaml_database_file)
 
     elif db_type == CONF_DB_TYPE_MONGODB:
-        # db_addr = config.address
-        # db_url = f"mongodb://{db_addr}:{db_port}/"
-
-        import os
-        # Get environment variables
-        db_username = os.getenv('MONGO_USERNAME')
-        db_password = os.environ.get('MONGO_PWD')
-
-        db_url = f'mongodb://{db_username}:{db_password}@mongodb-0.mongodb-headless:{MONGODB_PORT},mongodb-1.mongodb-headless:{MONGODB_PORT}/{DATABASE_NAME}'
+        db_url = get_mongodb_url()
 
         return MongoDB_Database(db_url)
 
@@ -37,15 +35,21 @@ def database_factory(config: DBConfig) -> Database:
         raise Exception(f"Database type {db_type} not supported.")
 
 
-def get_credentials():
+def get_mongodb_url():
     # Connect to the vault
-    client = hvac.Client(url=os.environ["VAULT_ADDR"], token=os.environ['VAULT_TOKEN'])
+    client = hvac.Client(
+        url=os.environ["VAULT_ADDR"],
+        token=os.environ['VAULT_TOKEN']
+    )
     mongodb_secret = client.secrets.kv.v2.read_secret_version(
-        mount_point=os.environ["VAULT_MOUNT"], path=f"{os.environ["VAULT_TOP_DIR"]}/{VAULT_NAME}"
+        mount_point=os.environ["VAULT_MOUNT"],
+        path=f'{os.environ["VAULT_TOP_DIR"]}/{VAULT_NAME}'
     )
 
     # Get environment variables
     db_username = mongodb_secret["data"]["data"]["MONGO_USERNAME"]
     db_password = mongodb_secret["data"]["data"]["MONGO_PASSWORD"]
 
-    return db_username, db_password
+    db_url = f'mongodb://{db_username}:{db_password}@mongodb-0.mongodb-headless:{MONGODB_PORT},mongodb-1.mongodb-headless:{MONGODB_PORT}/{DATABASE_NAME}'
+
+    return db_url

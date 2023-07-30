edf1cefbc798ce59b8555d584d69b7416efdda32
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2023-07-30 19:35:18 +0200

    private s3 db

--- src/private_database/utils.py
+++ src/private_database/utils.py
@@ -1,6 +1,6 @@
 from private_database.private_database import PrivateDatabase
 from private_database.constant_path import ConstantPath
-from utils.constants import CONSTANT_PATH_DB
+from utils.constants import CONSTANT_PATH_DB, S3_DB
 
 
 def private_database_factory(
@@ -9,10 +9,16 @@ def private_database_factory(
     """
     Returns the appropriate database class based on dataset storage location
     """
-    database_type = admin_database.get_database_type(dataset_name)
+    # database_type = admin_database.get_database_type(dataset_name)
+    database_type = admin_database.get_dataset_field(dataset_name, 'database_field')
 
     if database_type == CONSTANT_PATH_DB:
-        private_db = ConstantPath(dataset_name)
+        dataset_path = admin_database.get_dataset_field(dataset_name, 'dataset_path')
+        private_db = ConstantPath(dataset_path)
+    elif database_type == S3_DB:
+        s3_bucket = admin_database.get_dataset_field(dataset_name, 'bucket')
+        s3_prefix = admin_database.get_dataset_field(dataset_name, 'prefix')
+        private_db = S3Database(s3_bucket, s3_prefix)
     else:
         raise (
             f"Unknown database type {database_type} \

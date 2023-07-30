9d19369354d01faea1f1e06b7c2ba83ef161cae2
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2023-07-30 19:41:28 +0200

    formatting

--- src/private_database/utils.py
+++ src/private_database/utils.py
@@ -1,5 +1,6 @@
 from private_database.private_database import PrivateDatabase
 from private_database.constant_path import ConstantPath
+from private_database.s3_database import S3Database
 from utils.constants import CONSTANT_PATH_DB, S3_DB
 
 
@@ -10,14 +11,18 @@ def private_database_factory(
     Returns the appropriate database class based on dataset storage location
     """
     # database_type = admin_database.get_database_type(dataset_name)
-    database_type = admin_database.get_dataset_field(dataset_name, 'database_field')
+    database_type = admin_database.get_dataset_field(
+        dataset_name, "database_field"
+    )
 
     if database_type == CONSTANT_PATH_DB:
-        dataset_path = admin_database.get_dataset_field(dataset_name, 'dataset_path')
+        dataset_path = admin_database.get_dataset_field(
+            dataset_name, "dataset_path"
+        )
         private_db = ConstantPath(dataset_path)
     elif database_type == S3_DB:
-        s3_bucket = admin_database.get_dataset_field(dataset_name, 'bucket')
-        s3_prefix = admin_database.get_dataset_field(dataset_name, 'prefix')
+        s3_bucket = admin_database.get_dataset_field(dataset_name, "bucket")
+        s3_prefix = admin_database.get_dataset_field(dataset_name, "prefix")
         private_db = S3Database(s3_bucket, s3_prefix)
     else:
         raise (

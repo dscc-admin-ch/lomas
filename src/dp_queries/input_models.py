2ba75f74b8aba96b2580be5bebdde816e38cee5c
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2023-09-13 16:04:17 +0200

    adding dummy

--- src/dp_queries/input_models.py
+++ src/dp_queries/input_models.py
@@ -1,6 +1,5 @@
-import globals
-from typing import List
-from pydantic import BaseModel, validator, Field
+# from typing import List
+from pydantic import BaseModel, Field  # validator
 from utils.constants import EPSILON_LIMIT, DELTA_LIMIT
 import json
 
@@ -23,65 +22,6 @@ class BasicModel(BaseModel):
             if type(o).__name__ == "datetime"
             else o.__dict__,
         )
-
-
-# class OpenDPBase(BasicModel):
-#     function: str
-#     args: List
-#     kwargs: dict
-
-
-# class OpenDPAST(BasicModel):
-#     func: str
-#     module: str
-#     type: str
-#     args: dict = {}
-#     kwargs: dict = {}
-
-
-class OpenDPInp(BasicModel):
-    dataset_name: str
-    opendp_json: str  # TODO: improve later
-
-
-class DiffPrivLibModel(BasicModel):
-    name: str
-    type: str
-
-    # Could take single epsilon value and divide by len of pipeline
-    # to set epsilon for individual pipeline
-    # epsilon: float = Field(..., gt=0, le=10)
-
-    params: dict
-
-    # @validator('type')
-    # def validate_model(cls, type):
-    #     if type not in diffpriv_map.keys():
-    #         raise ValueError(f"'{type}' is not \
-    #                one of {list(diffpriv_map.keys())}.")
-    #     return type
-
-
-class DiffPrivLibInp(BasicModel):
-    module: str
-    pipeline: List[DiffPrivLibModel]
-    version: str
-    y_column: str
-
-    @validator("y_column")
-    def valid_Y(cls, y_column):
-        if y_column not in globals.TRAIN_Y.columns:
-            raise ValueError(
-                f"Provided y value '{y_column}' is not available. \
-                    Please select one of {list(globals.TRAIN_Y.columns)}."
-            )
-        return y_column
-
-    @validator("module")
-    def valid_module(cls, module):
-        if module != "diffprivlib":
-            raise ValueError(f"'{module}' is not diffprivlib.")
-        return module
 
 
 class GetDatasetMetadata(BasicModel):
@@ -120,3 +60,69 @@ class DummySNSQLInp(BasicModel):
 
 class GetBudgetInp(BaseModel):
     dataset_name: str
+
+
+# class OpenDPBase(BasicModel):
+#     function: str
+#     args: List
+#     kwargs: dict
+
+
+# class OpenDPAST(BasicModel):
+#     func: str
+#     module: str
+#     type: str
+#     args: dict = {}
+#     kwargs: dict = {}
+
+
+class OpenDPInp(BasicModel):
+    dataset_name: str
+    opendp_json: str  # TODO: improve with OpenDPAST and OpenDPBase
+
+
+class DummyOpenDPInp(BasicModel):
+    dataset_name: str
+    opendp_json: str  # TODO: improve with OpenDPAST and OpenDPBase
+    dummy_nb_rows: int
+    dummy_seed: int
+
+
+# class DiffPrivLibModel(BasicModel):
+#     name: str
+#     type: str
+
+#     # Could take single epsilon value and divide by len of pipeline
+#     # to set epsilon for individual pipeline
+#     # epsilon: float = Field(..., gt=0, le=10)
+
+#     params: dict
+
+#     # @validator('type')
+#     # def validate_model(cls, type):
+#     #     if type not in diffpriv_map.keys():
+#     #         raise ValueError(f"'{type}' is not \
+#     #                one of {list(diffpriv_map.keys())}.")
+#     #     return type
+
+
+# class DiffPrivLibInp(BasicModel):
+#     module: str
+#     pipeline: List[DiffPrivLibModel]
+#     version: str
+#     y_column: str
+
+#     @validator("y_column")
+#     def valid_Y(cls, y_column):
+#         if y_column not in globals.TRAIN_Y.columns:
+#             raise ValueError(
+#                 f"Provided y value '{y_column}' is not available. \
+#                     Please select one of {list(globals.TRAIN_Y.columns)}."
+#             )
+#         return y_column
+
+#     @validator("module")
+#     def valid_module(cls, module):
+#         if module != "diffprivlib":
+#             raise ValueError(f"'{module}' is not diffprivlib.")
+#         return module

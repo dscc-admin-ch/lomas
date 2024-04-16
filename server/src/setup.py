e9647d4c5b8d637fba0bb5845c8f7718b3a147a8
Lancelot Marti <lancelot.marti@bfs.admin.ch>
2024-04-16 10:17:01 +0000

    Rename surviving SDD

--- server/src/setup.py
+++ server/src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="lomas_server_package",
-    description="SDD POC Server Package.",
+    description="Lomas Server Package.",
     version="0.0.1",
     author="DSCC",
     author_email="DSCC@bfs.admin.ch",

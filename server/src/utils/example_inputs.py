7c6b5060609a02beea3e81ab3cdd0ef48bec3911
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2024-04-24 15:20:16 +0200

    more unit tests

--- server/src/utils/example_inputs.py
+++ server/src/utils/example_inputs.py
@@ -7,31 +7,31 @@ DUMMY_NB_ROWS = 100
 DUMMY_SEED = 42
 
 # DUMMY dataset name
-IRIS_DATASET = "IRIS"
+PENGUIN_DATASET = "PENGUIN"
 
 
 example_get_admin_db_data = {
-    "dataset_name": IRIS_DATASET,
+    "dataset_name": PENGUIN_DATASET,
 }
 
 example_get_dummy_dataset = {
-    "dataset_name": IRIS_DATASET,
+    "dataset_name": PENGUIN_DATASET,
     "dummy_nb_rows": DUMMY_NB_ROWS,
     "dummy_seed": DUMMY_SEED,
 }
 
 example_smartnoise_sql = {
     "query_str": "SELECT COUNT(*) AS NB_ROW FROM Schema.Table",
-    "dataset_name": IRIS_DATASET,
+    "dataset_name": PENGUIN_DATASET,
     "epsilon": 0.1,
     "delta": 0.00001,
-    "mechanisms": {"count": "discrete_gaussian"},
+    "mechanisms": {"count": "gaussian"},
     "postprocess": True,
 }
 
 example_dummy_smartnoise_sql = {
     "query_str": "SELECT COUNT(*) AS NB_ROW FROM Schema.Table",
-    "dataset_name": IRIS_DATASET,
+    "dataset_name": PENGUIN_DATASET,
     "epsilon": DUMMY_EPSILON,
     "delta": DUMMY_DELTA,
     "mechanisms": {"count": "discrete_gaussian"},
@@ -42,14 +42,14 @@ example_dummy_smartnoise_sql = {
 
 example_smartnoise_sql_cost = {
     "query_str": "SELECT COUNT(*) AS NB_ROW FROM Schema.Table",
-    "dataset_name": IRIS_DATASET,
+    "dataset_name": PENGUIN_DATASET,
     "epsilon": 0.1,
     "delta": 0.00001,
     "mechanisms": {"count": "discrete_gaussian"},
 }
 
 example_opendp = {
-    "dataset_name": IRIS_DATASET,
+    "dataset_name": PENGUIN_DATASET,
     "opendp_json": {
         "version": "0.8.0",
         "ast": {
@@ -89,7 +89,7 @@ example_opendp = {
 }
 
 example_dummy_opendp = {
-    "dataset_name": IRIS_DATASET,
+    "dataset_name": PENGUIN_DATASET,
     "opendp_json": {
         "version": "0.8.0",
         "ast": {
--- server/src/utils/output_models.py
+++ server/src/utils/output_models.py
@@ -1,13 +1,16 @@
 from pydantic import BaseModel
 
+
 class ServerState(BaseModel):
     state: list
     message: list
     LIVE: bool
 
+
 class State(BaseModel):
     requested_by: str
     state: ServerState
+
 
 class InitialBudget(BaseModel):
     initial_epsilon: float

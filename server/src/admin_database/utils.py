e7ec334100fe5666fa6f2ba5031ff3c87ac3f5ad
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2024-04-30 08:46:53 +0200

    putting yaml db back in, for exemple and testing purpose

--- server/src/admin_database/utils.py
+++ server/src/admin_database/utils.py
@@ -1,6 +1,7 @@
 from admin_database.admin_database import AdminDatabase
 from admin_database.mongodb_database import AdminMongoDatabase
-from constants import CONF_DB_TYPE_MONGODB
+from admin_database.yaml_database import AdminYamlDatabase
+from constants import AdminDBType
 from utils.config import Config, DBConfig
 from utils.error_handler import InternalServerException
 
@@ -12,15 +13,20 @@ def database_factory(config: DBConfig) -> AdminDatabase:
     """
     db_type = config.db_type
 
-    if db_type == CONF_DB_TYPE_MONGODB:
-        db_url = get_mongodb_url(config)
-        db_name = config.db_name
+    match db_type:
+        case AdminDBType.MONGODB_TYPE:
+            db_url = get_mongodb_url(config)
+            db_name = config.db_name
+            return AdminMongoDatabase(db_url, db_name)
 
-        return AdminMongoDatabase(db_url, db_name)
-    else:
-        raise InternalServerException(
-            f"Database type {db_type} not supported."
-        )
+        case AdminDBType.YAML_TYPE:
+            yaml_database_file = config.db_file
+            return AdminYamlDatabase(yaml_database_file)
+
+        case _:
+            raise InternalServerException(
+                f"Database type {db_type} not supported."
+            )
 
 
 def get_mongodb_url(config: Config) -> str:
--- server/src/admin_database/yaml_database.py
+++ server/src/admin_database/yaml_database.py
@@ -1,19 +1,19 @@
+from datetime import datetime
 import time
+import yaml
 from typing import List
 
 from admin_database.admin_database import AdminDatabase
 from constants import DPLibraries
-from pymongo import MongoClient
-from pymongo.database import Database
 from utils.error_handler import InternalServerException
 
 
-class AdminMongoDatabase(AdminDatabase):
+class AdminYamlDatabase(AdminDatabase):
     """
     Overall MongoDB database management
     """
 
-    def __init__(self, connection_string: str, database_name: str) -> None:
+    def __init__(self, yaml_db_path: str) -> None:
         """
         Load DB
 
@@ -21,7 +21,9 @@ class AdminMongoDatabase(AdminDatabase):
             - connection_string: Connection string to the mongodb
             - database_name: Mongodb database name.
         """
-        self.db: Database = MongoClient(connection_string)[database_name]
+        self.path: str = yaml_db_path
+        with open(yaml_db_path, "r") as f:
+            self.database = yaml.safe_load(f)
 
     def does_user_exist(self, user_name: str) -> bool:
         """
@@ -29,10 +31,11 @@ class AdminMongoDatabase(AdminDatabase):
         Parameters:
             - user_name: name of the user to check
         """
-        doc_count = self.db.users.count_documents(
-            {"user_name": f"{user_name}"}
-        )
-        return True if doc_count > 0 else False
+        for user in self.database["users"]:
+            if user["user_name"] == user_name:
+                return True
+
+        return False
 
     def does_dataset_exist(self, dataset_name: str) -> bool:
         """
@@ -40,9 +43,8 @@ class AdminMongoDatabase(AdminDatabase):
         Parameters:
             - dataset_name: name of the dataset to check
         """
-        collection_query = self.db.datasets.find({})
-        for document in collection_query:
-            if document["dataset_name"] == dataset_name:
+        for dt in self.database["datasets"]:
+            if dt["dataset_name"] == dataset_name:
                 return True
 
         return False
@@ -54,10 +56,14 @@ class AdminMongoDatabase(AdminDatabase):
         Parameters:
             - dataset_name: name of the dataset to get the metadata for
         """
-        metadatas = self.db.metadata.find_one(
-            {dataset_name: {"$exists": True}}
-        )
-        return metadatas[dataset_name]  # type: ignore
+        for dt in self.database["datasets"]:
+            if dt["dataset_name"] == dataset_name:
+                metadata_path = dt["metadata"]["metadata_path"]
+
+        with open(metadata_path, "r") as f:
+            metadata = yaml.safe_load(f)
+
+        return metadata
 
     @AdminDatabase._does_user_exist
     def may_user_query(self, user_name: str) -> bool:
@@ -67,8 +73,9 @@ class AdminMongoDatabase(AdminDatabase):
         Parameters:
             - user_name: name of the user
         """
-        user = self.db.users.find_one({"user_name": user_name})
-        return user["may_query"]  # type: ignore
+        for user in self.database["users"]:
+            if user["user_name"] == user_name:
+                return user["may_query"]
 
     @AdminDatabase._does_user_exist
     def set_may_user_query(self, user_name: str, may_query: bool) -> None:
@@ -79,10 +86,11 @@ class AdminMongoDatabase(AdminDatabase):
             - user_name: name of the user
             - may_query: flag give or remove access to user
         """
-        self.db.users.update_one(
-            {"user_name": f"{user_name}"},
-            {"$set": {"may_query": may_query}},
-        )
+        users = self.database["users"]
+        for user in users:
+            if user["user_name"] == user_name:
+                user["may_query"] = may_query
+        self.database["users"] = users
 
     @AdminDatabase._does_user_exist
     def has_user_access_to_dataset(
@@ -94,13 +102,12 @@ class AdminMongoDatabase(AdminDatabase):
             - user_name: name of the user
             - dataset_name: name of the dataset
         """
-        doc_count = self.db.users.count_documents(
-            {
-                "user_name": f"{user_name}",
-                "datasets_list.dataset_name": f"{dataset_name}",
-            }
-        )
-        return True if doc_count > 0 else False
+        for user in self.database["users"]:
+            if user["user_name"] == user_name:
+                for dataset in user["datasets_list"]:
+                    if dataset["dataset_name"] == dataset_name:
+                        return True
+        return False
 
     def get_epsilon_or_delta(
         self, user_name: str, dataset_name: str, parameter: str
@@ -113,19 +120,11 @@ class AdminMongoDatabase(AdminDatabase):
             - dataset_name: name of the dataset
             - parameter: total_spent_epsilon or total_spent_delta
         """
-        return list(
-            self.db.users.aggregate(
-                [
-                    {"$unwind": "$datasets_list"},
-                    {
-                        "$match": {
-                            "user_name": f"{user_name}",
-                            "datasets_list.dataset_name": f"{dataset_name}",
-                        }
-                    },
-                ]
-            )
-        )[0]["datasets_list"][parameter]
+        for user in self.database["users"]:
+            if user["user_name"] == user_name:
+                for dataset in user["datasets_list"]:
+                    if dataset["dataset_name"] == dataset_name:
+                        return dataset[parameter]
 
     def update_epsilon_or_delta(
         self,
@@ -143,13 +142,13 @@ class AdminMongoDatabase(AdminDatabase):
             - parameter: current_epsilon or current_delta
             - spent_value: spending of epsilon or delta on last query
         """
-        self.db.users.update_one(
-            {
-                "user_name": f"{user_name}",
-                "datasets_list.dataset_name": f"{dataset_name}",
-            },
-            {"$inc": {f"datasets_list.$.{parameter}": spent_value}},
-        )
+        users = self.database["users"]
+        for user in users:
+            if user["user_name"] == user_name:
+                for dataset in user["datasets_list"]:
+                    if dataset["dataset_name"] == dataset_name:
+                        dataset[parameter] += spent_value
+        self.database["users"] = users
 
     @AdminDatabase._does_dataset_exist
     def get_dataset_field(self, dataset_name: str, key: str) -> str:
@@ -159,8 +158,9 @@ class AdminMongoDatabase(AdminDatabase):
             - dataset_name: name of the dataset
             - key: name of the field to get
         """
-        dataset = self.db.datasets.find_one({"dataset_name": dataset_name})
-        return dataset[key]  # type: ignore
+        for dt in self.database["datasets"]:
+            if dt["dataset_name"] == dataset_name:
+                return dt[key]
 
     @AdminDatabase._has_user_access_to_dataset
     def get_user_previous_queries(
@@ -174,14 +174,14 @@ class AdminMongoDatabase(AdminDatabase):
             - user_name: name of the user
             - dataset_name: name of the dataset
         """
-        queries = self.db.queries_archives.find(
-            {
-                "user_name": f"{user_name}",
-                "dataset_name": f"{dataset_name}",
-            },
-            {"_id": 0},
-        )
-        return [q for q in queries]
+        previous_queries = []
+        for q in self.database["queries"]:
+            if (
+                q["user_name"] == user_name
+                and q["dataset_name"] == dataset_name
+            ):
+                previous_queries.append(q)
+        return previous_queries
 
     def save_query(
         self, user_name: str, query_json: dict, response: dict
@@ -211,4 +211,16 @@ class AdminMongoDatabase(AdminDatabase):
                     f"Unknown query input: {query_json.__class__.__name__}"
                 )
 
-        self.db.queries_archives.insert_one(to_archive)
+        self.database["queries"].append(to_archive)
+
+    def save_current_database(self) -> None:
+        """
+        Saves the current database with updated parameters in new yaml
+        with the date and hour in the path
+        Might be useful to verify state of DB during development
+        """
+        new_path = self.path.replace(
+            ".yaml", f'{datetime.now().strftime("%m_%d_%Y__%H_%M_%S")}.yaml'
+        )
+        with open(new_path, "w") as file:
+            yaml.dump(self.database, file)

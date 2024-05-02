ec5000a1e23721146d88bcaa464841e69c0bcc4d
Lancelot Marti <lancelot.marti@bfs.admin.ch>
2024-05-02 14:29:32 +0000

    Add different test for dummy generation, correct make_dummy_dataset

--- server/src/dp_queries/dummy_dataset.py
+++ server/src/dp_queries/dummy_dataset.py
@@ -59,7 +59,7 @@ def make_dummy_dataset(
             else:
                 serie = pd.Series(random.choices(RANDOM_STRINGS, k=nb_rows))
         elif col_type == "boolean":
-            serie = pd.Series(random.choices([True, False], k=nb_rows))
+            serie = pd.Series(random.choices([True, False], k=nb_rows), dtype = "boolean")
         elif col_type in ["int", "float"]:
             column_min = (
                 data["lower"]
@@ -72,19 +72,19 @@ def make_dummy_dataset(
                 else DEFAULT_NUMERICAL_MAX
             )
             if col_type == "int":
-                serie = np.random.randint(column_min, column_max, size=nb_rows)
+                serie = pd.Series(np.random.randint(column_min, column_max, size=nb_rows))
             else:
-                serie = np.random.uniform(column_min, column_max, size=nb_rows)
+                serie = pd.Series(np.random.uniform(column_min, column_max, size=nb_rows))
         elif col_type == "datetime":
             # From start date and random on a range above
-            start = datetime.strptime(RANDOM_DATE_START, "%m/%d/%Y")
-            serie = [
+            start = datetime.datetime.strptime(RANDOM_DATE_START, "%m/%d/%Y")
+            serie = pd.Series([
                 start
                 + datetime.timedelta(
                     seconds=random.randrange(RANDOM_DATE_RANGE)
                 )
                 for _ in range(nb_rows)
-            ]
+            ])
         elif col_type == "unknown":
             # Unknown column are ignored by snartnoise sql
             continue
@@ -98,10 +98,12 @@ def make_dummy_dataset(
         nullable = data["nullable"] if "nullable" in data.keys() else False
 
         if nullable:
-            for x in range(0, NB_RANDOM_NONE):
-                serie.insert(random.randrange(0, len(serie) - 1), None)
-            serie = serie[:-NB_RANDOM_NONE]
-
+            # Get the indexes of 'serie'
+            indexes = serie.index.tolist()
+            for _ in range(0, NB_RANDOM_NONE):
+                index_to_insert = random.choice(indexes)
+                serie.at[index_to_insert] = None
+                
         # Add randomly generated data as new column of dataframe
         df[col_name] = serie
 

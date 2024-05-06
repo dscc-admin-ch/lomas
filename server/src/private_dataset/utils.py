c3fa9c05d73b2789d86158bf70ede1b2963b1071
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2024-05-06 15:01:16 +0200

    remove duplicate file with 2 names

--- server/src/private_dataset/utils.py
+++ server/src/private_dataset/utils.py
@@ -1,8 +1,7 @@
 from admin_database.admin_database import AdminDatabase
 from constants import PrivateDatabaseType
-from private_dataset.local_dataset import LocalDataset
 from private_dataset.private_dataset import PrivateDataset
-from private_dataset.remote_http_dataset import RemoteHTTPDataset
+from private_dataset.path_dataset import PathDataset
 from private_dataset.s3_dataset import S3Dataset
 from utils.error_handler import InternalServerException
 
@@ -20,11 +19,11 @@ def private_dataset_factory(
     ds_metadata = admin_database.get_dataset_metadata(dataset_name)
 
     match database_type:
-        case PrivateDatabaseType.REMOTE_HTTP:
-            dataset_url = admin_database.get_dataset_field(
-                dataset_name, "dataset_url"
+        case PrivateDatabaseType.PATH:
+            dataset_path = admin_database.get_dataset_field(
+                dataset_name, "dataset_path"
             )
-            private_db = RemoteHTTPDataset(ds_metadata, dataset_url)
+            private_db = PathDataset(ds_metadata, dataset_path)
         case PrivateDatabaseType.S3:
             s3_bucket = admin_database.get_dataset_field(
                 dataset_name, "s3_bucket"
@@ -47,11 +46,6 @@ def private_dataset_factory(
                 s3_aws_access_key_id,
                 s3_aws_secret_access_key,
             )
-        case PrivateDatabaseType.LOCAL:
-            dataset_path = admin_database.get_dataset_field(
-                dataset_name, "dataset_path"
-            )
-            private_db = LocalDataset(ds_metadata, dataset_path)
         case _:
             raise InternalServerException(
                 f"Unknown database type: {database_type}"

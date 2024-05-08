6b5d7dcafcf554e0a49964a1c18b33cb71e9668e
Lancelot Marti <lancelot.marti@bfs.admin.ch>
2024-05-08 09:14:06 +0000

    Flake + black

--- server/src/tests/test_mongodb_admin.py
+++ server/src/tests/test_mongodb_admin.py
@@ -13,33 +13,37 @@ from mongodb_admin import (
     del_dataset_to_user,
     set_budget_field,
     set_may_query,
-    show_user,
     drop_collection,
-    create_users_collection
+    create_users_collection,
 )
 from utils.config import Config, get_config, CONFIG_LOADER
 from tests.constants import ENV_MONGO_INTEGRATION
 
-@unittest.skipIf(not(bool(os.getenv(ENV_MONGO_INTEGRATION, False))), f"Not an MongoDB integration test: {ENV_MONGO_INTEGRATION} environment variable not set to True.")
+
+@unittest.skipIf(
+    not (bool(os.getenv(ENV_MONGO_INTEGRATION, False))),
+    f"""Not an MongoDB integration test: {ENV_MONGO_INTEGRATION}
+        environment variable not set to True.""",
+)
 class TestRootAPIEndpoint(unittest.TestCase):
 
     @classmethod
     def setUpClass(self) -> None:
         CONFIG_LOADER.load_config(
             config_path="tests/test_configs/test_config_mongo.yaml",
-            secrets_path="tests/test_configs/test_secrets.yaml"
+            secrets_path="tests/test_configs/test_secrets.yaml",
         )
 
         self.db_config: Config = get_config().admin_database
         db_url: str = get_mongodb_url(self.db_config)
         self.db: Database = MongoClient(db_url)[self.db_config.db_name]
-    
+
     def setUp(self) -> None:
         self.args = SimpleNamespace(**vars(get_config().admin_database))
 
     def tearDown(self) -> None:
         self.args = SimpleNamespace(**vars(get_config().admin_database))
-        
+
         self.args.collection = "metadata"
         drop_collection(self.args)
 
@@ -54,17 +58,16 @@ class TestRootAPIEndpoint(unittest.TestCase):
 
         self.args = None
 
-
     def test_add_user(self) -> None:
         self.args.user = "Tintin"
-        
+
         # Add user
         add_user(self.args)
 
         expected_user = {
             "user_name": self.args.user,
             "may_query": True,
-            "datasets_list": []
+            "datasets_list": [],
         }
 
         user_found = self.db.users.find_one({"user_name": "Tintin"})
@@ -82,7 +85,6 @@ class TestRootAPIEndpoint(unittest.TestCase):
         self.args.epsilon = 10
         self.args.delta = 0.02
 
-        
         add_user_with_budget(self.args)
         expected_user = {
             "user_name": self.args.user,
@@ -93,9 +95,9 @@ class TestRootAPIEndpoint(unittest.TestCase):
                     "initial_epsilon": self.args.epsilon,
                     "initial_delta": self.args.delta,
                     "total_spent_epsilon": 0.0,
-                    "total_spent_delta": 0.0
+                    "total_spent_delta": 0.0,
                 }
-            ]
+            ],
         }
 
         user_found = self.db.users.find_one({"user_name": "Tintin"})
@@ -122,7 +124,7 @@ class TestRootAPIEndpoint(unittest.TestCase):
         # Removing non-existing should raise error
         with self.assertRaises(ValueError):
             del_user(self.args)
-        
+
     def test_add_dataset_to_user(self) -> None:
         self.args.user = "Tintin"
         self.args.dataset = "Bijoux de la Castafiore"
@@ -140,9 +142,9 @@ class TestRootAPIEndpoint(unittest.TestCase):
                     "initial_epsilon": self.args.epsilon,
                     "initial_delta": self.args.delta,
                     "total_spent_epsilon": 0.0,
-                    "total_spent_delta": 0.0
+                    "total_spent_delta": 0.0,
                 }
-            ]
+            ],
         }
 
         user_found = self.db.users.find_one({"user_name": "Tintin"})
@@ -175,7 +177,7 @@ class TestRootAPIEndpoint(unittest.TestCase):
         expected_user = {
             "user_name": self.args.user,
             "may_query": True,
-            "datasets_list": []
+            "datasets_list": [],
         }
         user_found = self.db.users.find_one({"user_name": "Tintin"})
         del user_found["_id"]
@@ -187,14 +189,14 @@ class TestRootAPIEndpoint(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             del_dataset_to_user(self.args)
-        
+
         # Remove dataset not present in user should raise error
         self.args.user = "Tintin"
         self.args.dataset = "Bijoux de la Castafiore"
-        
+
         with self.assertRaises(Exception):
             del_dataset_to_user(self.args)
- 
+
     def test_set_budget_field(self) -> None:
         # Setup: add user with budget
         self.args.user = "Tintin"
@@ -202,9 +204,8 @@ class TestRootAPIEndpoint(unittest.TestCase):
         self.args.epsilon = 10
         self.args.delta = 0.02
 
-        
         add_user_with_budget(self.args)
-        
+
         # Updating budget should work
         self.args.field = "initial_epsilon"
         self.args.value = 15
@@ -219,9 +220,9 @@ class TestRootAPIEndpoint(unittest.TestCase):
                     "initial_epsilon": self.args.value,
                     "initial_delta": self.args.delta,
                     "total_spent_epsilon": 0.0,
-                    "total_spent_delta": 0.0
+                    "total_spent_delta": 0.0,
                 }
-            ]
+            ],
         }
 
         user_found = self.db.users.find_one({"user_name": "Tintin"})
@@ -231,7 +232,7 @@ class TestRootAPIEndpoint(unittest.TestCase):
 
         # Setting budget for non-existing user should fail
         self.args.user = "Milou"
-        
+
         with self.assertRaises(ValueError):
             set_budget_field(self.args)
 
@@ -264,9 +265,9 @@ class TestRootAPIEndpoint(unittest.TestCase):
                     "initial_epsilon": self.args.epsilon,
                     "initial_delta": self.args.delta,
                     "total_spent_epsilon": 0.0,
-                    "total_spent_delta": 0.0
+                    "total_spent_delta": 0.0,
                 }
-            ]
+            ],
         }
 
         user_found = self.db.users.find_one({"user_name": "Tintin"})
@@ -276,7 +277,7 @@ class TestRootAPIEndpoint(unittest.TestCase):
 
         # Raises error when user does not exist
         self.args.user = "Milou"
-        
+
         with self.assertRaises(ValueError):
             set_may_query(self.args)
 
@@ -296,9 +297,9 @@ class TestRootAPIEndpoint(unittest.TestCase):
                     "initial_epsilon": 10,
                     "initial_delta": 0.005,
                     "total_spent_epsilon": 0.0,
-                    "total_spent_delta": 0.0
+                    "total_spent_delta": 0.0,
                 }
-            ]
+            ],
         }
 
         user_found = self.db.users.find_one({"user_name": "Tintin"})
@@ -315,9 +316,9 @@ class TestRootAPIEndpoint(unittest.TestCase):
                     "initial_epsilon": 20,
                     "initial_delta": 0.005,
                     "total_spent_epsilon": 0.0,
-                    "total_spent_delta": 0.0
+                    "total_spent_delta": 0.0,
                 }
-            ]
+            ],
         }
 
         user_found = self.db.users.find_one({"user_name": "Milou"})
@@ -335,7 +336,7 @@ class TestRootAPIEndpoint(unittest.TestCase):
         del self.args.field
         del self.args.value
         del self.args.dataset
-        
+
         self.args.clean = True
         create_users_collection(self.args)
 
@@ -380,14 +381,3 @@ class TestRootAPIEndpoint(unittest.TestCase):
 
         with self.assertWarns(UserWarning):
             create_users_collection(self.args)
-
-
-
-        
-
-            
-
-
-
-
-        

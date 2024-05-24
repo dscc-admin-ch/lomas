f044820c6e1487d7c56ecec663ca74f8cc7f99e0
Lancelot Marti <lancelot.marti@bfs.admin.ch>
2024-05-24 07:24:35 +0000

    Flake8 limit to 88

--- client/docs/source/conf.py
+++ client/docs/source/conf.py
@@ -21,12 +21,12 @@ if scripts not in sys.path:
 
 # -- Project information -----------------------------------------------------
 
-project = 'Lomas-client'
-copyright = '2024, DSCC'
-author = 'DSCC'
+project = "Lomas-client"
+copyright = "2024, DSCC"
+author = "DSCC"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = "0.0.2"
 
 
 # -- General configuration ---------------------------------------------------
@@ -35,7 +35,10 @@ release = '0.0.2'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon", "nbsphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "nbsphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,12 +56,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-# The name of an image file (relative to this directory) to place at the top of the sidebar.
-html_logo = '_static/logo.png'
+# The name of an image file (relative to this directory)
+# to place at the top of the sidebar.
+html_logo = "_static/logo.png"

6a8684a608a4b98a73affe464e8e98b0e8217d4d
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2024-06-06 11:55:12 +0200

    true not True

--- server/run_tests.sh
+++ server/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ./lomas_server
-coverage run --rcfile=<(echo "[run]\nrelative_files = True") --source=. -m unittest discover -s .
+coverage run --rcfile=<(echo "[run]\nrelative_files = true") --source=. -m unittest discover -s .
 ret=$?
 coverage report
 coverage xml -o coverage.xml

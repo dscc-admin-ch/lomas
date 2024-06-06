9a73f75c59e08240eb707adc0a65cb794694f0d1
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2024-06-06 12:51:08 +0200

    run on test integration instead of test server

--- server/run_integration_tests.sh
+++ server/run_integration_tests.sh
@@ -6,8 +6,10 @@ sleep 3
 
 cd ./lomas_server
 export LOMAS_TEST_MONGO_INTEGRATION=1
-python -m unittest discover -s .
+coverage run --source=. -m unittest discover -s .
 ret=$?
+coverage report
+coverage xml -o coverage.xml
 cd ..
 
 docker compose -f docker-compose-test.yml down --volumes

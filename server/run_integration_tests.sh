1c9c7a371647227a77dd3ddc2a455719b6473f41
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2024-06-06 13:10:31 +0200

    different test case

--- server/run_integration_tests.sh
+++ server/run_integration_tests.sh
@@ -5,11 +5,20 @@ docker compose -f docker-compose-test.yml up --detach
 sleep 3
 
 cd ./lomas_server
+
+# On MongoDB Admin
 export LOMAS_TEST_MONGO_INTEGRATION=1
 coverage run --source=. -m unittest discover -s .
 ret=$?
+
+# On YamlDB Admin
+export LOMAS_TEST_MONGO_INTEGRATION=0
+coverage run -a --source=. -m unittest discover -s .
+ret=$?
+
 coverage report
 coverage xml -o coverage.xml
+
 cd ..
 
 docker compose -f docker-compose-test.yml down --volumes

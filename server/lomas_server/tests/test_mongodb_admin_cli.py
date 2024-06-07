bd00aac31a28b32188fde4821f5461c51e53396e
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2024-06-07 11:17:27 +0200

    testing

--- server/lomas_server/tests/test_mongodb_admin_cli.py
+++ server/lomas_server/tests/test_mongodb_admin_cli.py
@@ -359,7 +359,7 @@ class TestMongoDBAdmin(unittest.TestCase):  # pylint: disable=R0904
         )
 
         # Set may query
-        value = "false"
+        value = 0
         self.run_cli_command("set_may_query", ["-u", user, "-v", value])
 
         expected_user = {
@@ -397,8 +397,8 @@ class TestMongoDBAdmin(unittest.TestCase):  # pylint: disable=R0904
         """Test create user collection via YAML file via cli"""
         # Adding two users
         path = "./tests/test_data/test_user_collection.yaml"
-        clean = "false"
-        overwrite = "false"
+        clean = 0
+        overwrite = 0
         self.run_cli_command(
             "add_users_via_yaml", ["-c", clean, "-o", overwrite, "-yf", path]
         )
@@ -542,9 +542,9 @@ class TestMongoDBAdmin(unittest.TestCase):  # pylint: disable=R0904
             self.assertEqual(metadata_found, penguin_metadata)
 
         path = "./tests/test_data/test_datasets.yaml"
-        clean = "false"
-        overwrite_datasets = "false"
-        overwrite_metadata = "false"
+        clean = 0
+        overwrite_datasets = 0
+        overwrite_metadata = 0
 
         self.run_cli_command(
             "add_datasets_via_yaml",
@@ -665,11 +665,11 @@ class TestMongoDBAdmin(unittest.TestCase):  # pylint: disable=R0904
                 "--yaml_file",
                 "./tests/test_data/test_datasets.yaml",
                 "--clean",
-                "false",
+                0,
                 "--overwrite_datasets",
-                "false",
+                0,
                 "--overwrite_metadata",
-                "false",
+                0,
             ],
         )
         self.run_cli_command("get_list_of_datasets", [])
@@ -715,11 +715,11 @@ class TestMongoDBAdmin(unittest.TestCase):  # pylint: disable=R0904
                 "--yaml_file",
                 "./tests/test_data/test_datasets.yaml",
                 "--clean",
-                "false",
+                0,
                 "--overwrite_datasets",
-                "false",
+                0,
                 "--overwrite_metadata",
-                "false",
+                0,
             ],
         )
         self.run_cli_command("show_collection", ["-c", "datasets"])

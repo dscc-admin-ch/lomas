38c2fde8e992623e557f964647880cc780f37859
Pauline Maury Laribière <pauline.maury-laribiere@bfs.admin.ch>
2024-06-12 14:18:29 +0200

    fix test

--- server/run_tests_and_coverage.sh
+++ server/run_tests_and_coverage.sh
@@ -13,18 +13,18 @@ export LOMAS_TEST_S3_INTEGRATION=1 # tests with s3 minio available
 coverage run --source=. -m unittest discover -s .
 ret1=$?
 
-# # "yaml", "basic", developer mode, "stall"
-# export LOMAS_TEST_MONGO_INTEGRATION=0
-# export LOMAS_TEST_S3_INTEGRATION=0
-# coverage run -a --source=. -m unittest discover -s .
-# ret2=$?
+# "yaml", "basic", developer mode, "stall"
+export LOMAS_TEST_MONGO_INTEGRATION=0
+export LOMAS_TEST_S3_INTEGRATION=0
+coverage run -a --source=. -m unittest discover -s .
+ret2=$?
 
-# coverage report
-# coverage xml -o coverage.xml
+coverage report
+coverage xml -o coverage.xml
 
 cd ..
 
 docker compose -f docker-compose-test.yml down --volumes
 
-# ret=$((ret1 + ret2))
-exit $ret1
+ret=$((ret1 + ret2))
+exit $ret

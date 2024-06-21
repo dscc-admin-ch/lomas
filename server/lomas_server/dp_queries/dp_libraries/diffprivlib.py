219f994f11f4e9cb637598b4d5e07354d6e25fcd
PaulineMauryL <pauline.maury-laribiere@bfs.admin.ch>
2024-06-21 14:19:56 +0200

    split diffprivlib and re-work architecture

--- server/lomas_server/dp_queries/dp_libraries/diffprivlib.py
+++ server/lomas_server/dp_queries/dp_libraries/diffprivlib.py
@@ -1,24 +1,24 @@
-import base64
-import json
+from base64 import b64encode
 import pickle
 import warnings
 from typing import Dict
 
-import diffprivlib
-import pandas as pd
+from diffprivlib import models
 from diffprivlib.utils import (
     DiffprivlibCompatibilityWarning,
     PrivacyLeakWarning,
 )
-from fastapi import HTTPException
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
+import pandas as pd
 
-from constants import NUMERICAL_DTYPES
+from constants import DPLibraries
 from dp_queries.dp_querier import DPQuerier
-from private_dataset.private_dataset import PrivateDataset
-from utils.loggr import LOG
+from dp_queries.dp_libraries.diffprivlib_utils import (
+    impute_missing_data,
+    split_train_test_data,
+    deserialise_diffprivlib_pipeline,
+)
+from utils.error_handler import ExternalLibraryException
+from utils.input_models import DiffPrivLibInp
 
 # DiffPrivLib warnings will trigger error
 warnings.simplefilter("error", PrivacyLeakWarning)
@@ -26,96 +26,64 @@ warnings.simplefilter("error", DiffprivlibCompatibilityWarning)
 
 
 class DiffPrivLibQuerier(DPQuerier):
-    def __init__(
-        self,
-        private_dataset: PrivateDataset,
-    ) -> None:
-        super().__init__(private_dataset)
+    """
+    Concrete implementation of the DPQuerier ABC for the DiffPrivLib library.
+    """
 
-    def deserialise_pipeline(self, pipeline):
+    def fit_model_on_data(
+        self, query_json: DiffPrivLibInp
+    ) -> tuple[models, pd.DataFrame, pd.DataFrame]:
+        """Perform necessary steps to fit the model on the data
+
+        Args:
+            query_json (BaseModel): The JSON request object for the query.
+
+        Raises:
+            ExternalLibraryException: For exceptions from libraries
+                external to this package.
+
+        Returns:
+            fitted_dpl_pipeline (dpl model): the fitted model on the training data
+            x_test (pd.DataFrame): test data feature
+            y_test (pd.DataFrame): test data target
+        """
+        # Prepare data
+        raw_data = self.private_dataset.get_pandas_df()
+        data = impute_missing_data(raw_data, query_json.imputer_strategy)
+        x_train, x_test, y_train, y_test = split_train_test_data(
+            data, query_json
+        )
+
+        # Prepare DiffPrivLib pipeline
+        dpl_pipeline = deserialise_diffprivlib_pipeline(
+            query_json.diffprivlib_json
+        )
+
+        # Fit the pipeline on the training set
         try:
-            dpl_pipeline = deserialise_diffprivlib_pipeline(pipeline)
-        except ValueError as e:
-            raise HTTPException(400, str(e))
-        except Exception as exc:
-            LOG.error(exc)
-            raise exc
-        return dpl_pipeline
-
-    def prepare_data(self, query_json):
-        data = self.private_dataset.get_pandas_df()
-
-        imputer_strategy = query_json.imputer_strategy
-        if imputer_strategy == "drop":
-            data = data.dropna()
-        elif imputer_strategy in ["mean", "median"]:
-            numerical_cols = data.select_dtypes(
-                include=NUMERICAL_DTYPES
-            ).columns.tolist()
-            categorical_cols = [
-                col for col in data.columns if col not in numerical_cols
-            ]
-
-            # Impute numerical features using given strategy
-            imp_mean = SimpleImputer(strategy=imputer_strategy)
-            df_num_imputed = imp_mean.fit_transform(data[numerical_cols])
-
-            # Impute categorical features with most frequent value
-            imp_most_frequent = SimpleImputer(strategy="most_frequent")
-            df_cat_imputed = imp_most_frequent.fit_transform(
-                data[categorical_cols]
-            )
-
-            # Combine imputed dataframes
-            data = pd.concat(
-                [
-                    pd.DataFrame(df_num_imputed, columns=numerical_cols),
-                    pd.DataFrame(df_cat_imputed, columns=categorical_cols),
-                ],
-                axis=1,
-            )
-        elif imputer_strategy == "most_frequent":
-            # Impute all features with most frequent value
-            imp_most_frequent = SimpleImputer(strategy=imputer_strategy)
-            data = pd.DataFrame(
-                imp_most_frequent.fit_transform(data), columns=data.columns
-            )
-        else:
-            raise HTTPException(
-                500, f"Imputation strategy {imputer_strategy} not supported"
-            )
-
-        feature_data = data[query_json.feature_columns]
-
-        if query_json.target_columns is None:
-            x_train, x_test = train_test_split(
-                feature_data,
-                test_size=query_json.test_size,
-                random_state=query_json.test_train_split_seed,
-            )
-            y_train, y_test = None, None
-        else:
-            label_data = data[query_json.target_columns]
-            x_train, x_test, y_train, y_test = train_test_split(
-                feature_data,
-                label_data,
-                test_size=query_json.test_size,
-                random_state=query_json.test_train_split_seed,
-            )
-        return x_train, x_test, y_train, y_test
-
-    def fit_pipeline(self, pipeline, x_train, y_train):
-        try:
-            pipeline.fit(x_train, y_train)
+            fitted_dpl_pipeline = dpl_pipeline.fit(x_train, y_train)
         except Exception as e:
-            LOG.exception(f"Cannot train model error: {str(e)}")
-            raise HTTPException(500, f"Cannot train model error: {str(e)}")
-        return pipeline
+            raise ExternalLibraryException(
+                DPLibraries.DIFFPRIVLIB, "Cannot fit pipeline on data"
+            ) from e
 
-    def cost(self, query_json: dict) -> tuple[float, float]:
-        dpl_pipeline = self.deserialise_pipeline(query_json.diffprivlib_json)
-        x_train, _, y_train, _ = self.prepare_data(query_json)
-        fitted_dpl_pipeline = self.fit_pipeline(dpl_pipeline, x_train, y_train)
+        return fitted_dpl_pipeline, x_test, y_test
+
+    def cost(self, query_json: DiffPrivLibInp) -> tuple[float, float]:
+        """Estimate cost of query
+
+        Args:
+            query_json (BaseModel): The JSON request object for the query.
+
+        Raises:
+            ExternalLibraryException: For exceptions from libraries
+                external to this package.
+
+        Returns:
+            tuple[float, float]: The tuple of costs, the first value
+                is the epsilon cost, the second value is the delta value.
+        """
+        fitted_dpl_pipeline, _, _ = self.fit_model_on_data(query_json)
 
         # Compute budget
         spent_epsilon = 0.0
@@ -125,70 +93,33 @@ class DiffPrivLibQuerier(DPQuerier):
             spent_delta += step[1].accountant.spent_budget[0][1]
         return spent_epsilon, spent_delta
 
-    def query(self, query_json: dict) -> Dict:
-        dpl_pipeline = self.deserialise_pipeline(query_json.diffprivlib_json)
-        x_train, x_test, y_train, y_test = self.prepare_data(query_json)
-        fitted_dpl_pipeline = self.fit_pipeline(dpl_pipeline, x_train, y_train)
+    def query(self, query_json: DiffPrivLibInp) -> Dict:
+        """Perform the query and return the response.
+
+        Args:
+            query_json (BaseModel): The JSON request object for the query.
+
+        Raises:
+            ExternalLibraryException: For exceptions from libraries
+                external to this package.
+            InvalidQueryException: If the budget values are too small to
+                perform the query.
+
+        Returns:
+            dict: The dictionary encoding of the resulting pd.DataFrame.
+        """
+        fitted_dpl_pipeline, x_test, y_test = self.fit_model_on_data(
+            query_json
+        )
 
         # Model accuracy
         score = fitted_dpl_pipeline.score(x_test, y_test)
 
         # Serialise model
-        pickled_model = base64.b64encode(
-            pickle.dumps(fitted_dpl_pipeline)
-        ).decode("utf-8")
+        pickled_model = b64encode(pickle.dumps(fitted_dpl_pipeline))
 
-        query_response = {"score": score, "model": pickled_model}
+        query_response = {
+            "score": score,
+            "model": pickled_model.decode("utf-8"),
+        }
         return query_response
-
-
-class DiffPrivLibDecoder(json.JSONDecoder):
-    def __init__(self, *args, **kwargs):
-        json.JSONDecoder.__init__(
-            self, object_hook=self.object_hook, *args, **kwargs
-        )
-
-    def object_hook(self, dct):
-        if "_tuple" in dct.keys():
-            return tuple(dct["_items"])
-
-        for k, v in dct.items():
-            if type(v) is str:
-                if v[:10] == "_dpl_type:":
-                    try:
-                        dct[k] = getattr(diffprivlib.models, v[10:])
-                    except Exception as e:
-                        LOG.exception(e)
-
-                elif v[:14] == "_dpl_instance:":
-                    try:
-                        dct[k] = getattr(diffprivlib, v[14:])()
-                    except Exception as e:
-                        LOG.exception(e)
-
-        return dct
-
-
-def deserialise_diffprivlib_pipeline(diffprivlib_json):
-    dct = json.loads(diffprivlib_json, cls=DiffPrivLibDecoder)
-    if "module" in dct.keys():
-        if dct["module"] != "diffprivlib":
-            raise ValueError("JSON 'module' not equal to 'diffprivlib'")
-    else:
-        raise ValueError("Key 'module' not in submitted json request.")
-
-    if "version" in dct.keys():
-        if dct["version"] != diffprivlib.__version__:
-            raise ValueError(
-                f"Requested version does not match available version:"
-                f" {diffprivlib.__version__}."
-            )
-    else:
-        raise ValueError("Key 'version' not in submitted json request.")
-
-    return Pipeline(
-        [
-            (val["name"], val["type"](**val["params"]))
-            for val in dct["pipeline"]
-        ]
-    )

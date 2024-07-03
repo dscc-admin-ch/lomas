3e7ec25e492e85babdfb6f5efb8c2e76f673f7de
PaulineMauryL <pauline.maury-laribiere@bfs.admin.ch>
2024-07-03 19:24:12 +0200

    add pylint file for client

--- server/run_linter.sh
+++ server/run_linter.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-pip install isort==5.13.2
-pip install black==24.4.2
-pip install flake8==7.1.0
-pip install mypy==1.10.0
-pip install pylint==3.1.0
+install_dependencies() {
+    pip install isort==5.13.2
+    pip install black==24.4.2
+    pip install flake8==7.1.0
+    pip install mypy==1.10.0
+    pip install pylint==3.1.0
+}
 
+# Parse command line arguments for --install-deps flag
+INSTALL_DEPS=false
+for arg in "$@"; do
+    if [ "$arg" == "--install-deps" ]; then
+        INSTALL_DEPS=true
+        break
+    fi
+done
+
+# Install dependencies if flag is set
+if [ "$INSTALL_DEPS" == true ]; then
+    install_dependencies
+fi
+
+
+cd ./lomas_server
 isort .
 black .
 flake8 .
-mypy .
-
-cd ./lomas_server
 pylint .
+
+cd ..
+mypy .

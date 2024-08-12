332e8bb458c30faaba6cdd68795ce74c5d94f97e
PaulineMauryL <pauline.maury-laribiere@bfs.admin.ch>
2024-08-12 13:23:46 +0200

    fix mongidb test, add test for snsynth, remove mul_matrix

--- server/lomas_server/tests/constants.py
+++ server/lomas_server/tests/constants.py
@@ -2,3 +2,13 @@ ENV_MONGO_INTEGRATION = "LOMAS_TEST_MONGO_INTEGRATION"
 ENV_S3_INTEGRATION = "LOMAS_TEST_S3_INTEGRATION"
 TRUE_VALUES = ("true", "1", "t")
 FALSE_VALUES = ("false", "0", "f")
+
+PENGUIN_COLUMNS = [
+    "species",
+    "island",
+    "bill_length_mm",
+    "bill_depth_mm",
+    "flipper_length_mm",
+    "body_mass_g",
+    "sex",
+]

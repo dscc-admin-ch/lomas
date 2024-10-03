4a53f59b200f362b5762cb847df6549faad91315
PaulineMauryL <pauline.maury-laribiere@bfs.admin.ch>
2024-10-03 08:00:40 +0000

    about.py page done

--- server/lomas_server/administration/test_streamlit_app.py
+++ server/lomas_server/administration/test_streamlit_app.py
@@ -1,35 +1,35 @@
 import unittest
 from unittest.mock import patch
 import streamlit as st
+from st_pages import Page
+
 
 class TestAboutPage(unittest.TestCase):
 
     @patch("streamlit.write")
     @patch("streamlit.header")
     @patch("streamlit.title")
+    @patch("streamlit.set_page_config")
     @patch("st_pages.show_pages") 
-    def test_about_page(self, mock_show_pages, mock_title, mock_header, mock_write):
-        # Mock show_pages since it depends on the Streamlit runtime
-        mock_show_pages.return_value = None
-
-        # Import the about.py file and call the main() function
+    def test_about_page(self, mock_show_pages, mock_set_page_config, mock_title, mock_header, mock_write):
         from lomas_server.administration.dashboard import about
         about.main()
 
-        # Assert that the title is called once with "Welcome!"
-        mock_title.assert_called_once_with("Welcome!")
+        mock_set_page_config.assert_called_once_with(page_title="Lomas Dashboard")
 
-        # Assert that the headers are called with the expected text
+        expected_pages = [
+            Page("./lomas_server/administration/dashboard/about.py", "Home Page", "🏠"),
+            Page("./lomas_server/administration/dashboard/pages/01_server_overview.py", "Lomas server overview", ":computer:"),
+            Page("./lomas_server/administration/dashboard/pages/02_database_administration.py", "Admin database management", ":file_folder:")
+        ]
+        mock_show_pages.assert_called_once_with(expected_pages)
+
+        mock_title.assert_called_once_with("Welcome!")
         mock_header.assert_any_call("Lomas Administation Dashboard")
         mock_header.assert_any_call("Key Features")
 
-        # Assert that some text has been written to the page
-        mock_write.assert_called()  # At least one write should be called
-
         # Get all write calls
         written_texts = [call.args[0] for call in mock_write.call_args_list]
-
-        # Assert that the expected text is somewhere in the calls
         self.assertTrue(
             any("The Lomas Administration Dashboard provides a centralized interface" in text for text in written_texts)
         )

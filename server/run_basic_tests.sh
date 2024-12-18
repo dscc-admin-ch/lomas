c7e02cec7909e6866254684f31a7d1d6f40af78b
Lancelot Marti <lancelot.marti@bfs.admin.ch>
2024-12-18 07:34:09 +0000

    Refactore tests (add utils file, rename file), Correct tests script

--- server/run_basic_tests.sh
+++ server/run_basic_tests.sh
@@ -8,9 +8,12 @@ export PYTHONPATH=$(pwd):$PYTHONPATH
 
 cd lomas_server/
 python -m unittest discover -s .
-ret=$?
+ret1=$?
 pytest ./administration/tests/test_streamlit_app.py
-pytest ./administration/tests/test_streamlit_app_b.py
+ret2=$?
+pytest ./administration/tests/test_streamlit_app_page_b.py
+ret3=$?
 cd ..
 
+ret=$((ret1 + ret2 + ret3))
 exit $ret

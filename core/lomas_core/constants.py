6365e973ae5cf2e666e7460056c3e68c5aa87bd0
pauline.maury-laribiere <pauline.maury-laribiere@bfs.admin.ch>
2025-02-06 14:00:06 +0000

    add values.schema.json where relevant

--- core/lomas_core/constants.py
+++ core/lomas_core/constants.py
@@ -1,7 +1,7 @@
 import os
 from enum import StrEnum
 
-OTLP_COLLECTOR_ENDPOINT = os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317")
+OTLP_COLLECTOR_ENDPOINT = os.getenv("OTEL_COLLECTOR_ENDPOINT", "http://localhost:4317")
 
 # Server error messages
 INTERNAL_SERVER_ERROR = "Internal server error. Please contact the administrator of this service."

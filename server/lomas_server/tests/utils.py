90f06b953e31d2a3e916139d19402f854533a56f
Bruno Studer <bruno.studer@protonmail.com>
2025-02-27 10:33:14 +0100

    [test] longer timeout & simplify active wait

--- server/lomas_server/tests/utils.py
+++ server/lomas_server/tests/utils.py
@@ -1,14 +1,14 @@
 from json import loads
-from time import sleep
+from test.support import sleeping_retry
 
 from fastapi import status
 
 from lomas_core.models.responses import Job
 
 
-def wait_for_job(client, endpoint, n_retry=100, sleep_sec=0.5) -> Job:
+def wait_for_job(client, endpoint) -> Job:
     """Periodically query the job endpoint sleeping in between until it completes / times-out."""
-    for _ in range(n_retry):
+    for _ in sleeping_retry(75, error=False):
         job_query = client.get(endpoint).json()
 
         if job_query["status"] == "complete":
@@ -17,17 +17,15 @@ def wait_for_job(client, endpoint, n_retry=100, sleep_sec=0.5) -> Job:
         if (job_err := job_query.get("error")) is not None:
             return Job.model_validate(job_query | {"error": loads(job_err)})
 
-        sleep(sleep_sec)
-
-    raise TimeoutError(f"Job {endpoint} didn't complete in time ({sleep_sec * n_retry})")
+    raise TimeoutError(f"Job {endpoint} didn't complete in time")
 
 
-def submit_job_wait(client, endpoint, json, headers=None, **kwargs) -> Job:
+def submit_job_wait(client, endpoint, json, headers=None) -> Job:
     """Post to a Job-type endpoint and periodically wait for result."""
     query_job_submit = client.post(endpoint, json=json, headers=headers)
     assert query_job_submit.status_code == status.HTTP_202_ACCEPTED
 
     job_uid = query_job_submit.json()["uid"]
-    job = wait_for_job(client, f"/status/{job_uid}", **kwargs)
+    job = wait_for_job(client, f"/status/{job_uid}")
 
     return job

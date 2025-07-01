1ebea782f927d17c6aa89289d6f62e09206ce841
Lancelot Marti <lancelot.marti@bfs.admin.ch>
2025-07-01 06:47:56 +0000

    Print small warning for datetime in client

--- client/lomas_client/client.py
+++ client/lomas_client/client.py
@@ -122,6 +122,11 @@ class Client:
                 # Will be fixed in 0.13
                 for col in dummy_df.select_dtypes(include=["datetime"]):
                     dummy_df[col] = dummy_df[col].astype("string[python]")
+                print(
+                    "Datetime type mismatch: The Polars LazyFrame currently uses 'str' for datetime fields, "
+                    "which may not match the expected metadata types. This is a temporary workaround and will be "
+                    "resolved in a future release (>=0.13)."
+                )
                 return pl.from_pandas(dummy_df).lazy()
             return dummy_df
 

47dcf9c28e6c1a8f99e574ed7cfbe020baecaa20
pauline.maury-laribiere <pauline.maury-laribiere@bfs.admin.ch>
2025-07-14 14:10:31 +0000

    add snsql metadata

--- server/lomas_server/dp_queries/dp_libraries/smartnoise_sql.py
+++ server/lomas_server/dp_queries/dp_libraries/smartnoise_sql.py
@@ -173,6 +173,8 @@ def convert_to_smartnoise_metadata(metadata: Metadata) -> dict:
             for k in ["lower", "upper"]:
                 if val.get(k) is not None:
                     del val[k]
+        if hasattr(val, "nullable_proportion"):
+            val["nullable"] = val["nullable_proportion"] > 0
 
     metadata_dict.update(metadata_dict["columns"])
     del metadata_dict["columns"]

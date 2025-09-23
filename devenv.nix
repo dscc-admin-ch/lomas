cde02d0b9d26cd7723a76dcc22174f538ad489cf
Bruno Studer <bruno.studer@protonmail.com>
2025-09-23 08:18:08 +0000

    Devenv update (#335)
    * devenv bump 1.6 -> 1.9
    * processes as tasks now avail.
    * removed rule
    * PC condition update

--- devenv.nix
+++ devenv.nix
@@ -183,125 +183,123 @@ in
   dockerEnv.enable = true;
 
   # Environment variable available inside devenv
-  env =
+  env = {
+    GREET = "Lomas env";
+
+    # Ensure `coverage` uses our project config
+    COVERAGE_RCFILE = config.lomas.hooks.projectConfigFile;
+
+    # Pydantic note:
+    # Even when using a dotenv file, pydantic will still read environment variables as well as the dotenv file, environment variables will always take priority over values loaded from a dotenv file.
+
+    # Lomas Server Runtime
+    LOMAS_SERVICE_server__host_ip = config.lomas.host;
+    LOMAS_SERVICE_server__host_port = config.lomas.port;
+    LOMAS_SERVICE_server__log_level = "info";
+    LOMAS_SERVICE_server__reload = "true";
+    LOMAS_SERVICE_server__submit_limit = 300;
+    LOMAS_SERVICE_server__time_attack__method = "jitter";
+    LOMAS_SERVICE_server__time_attack__magnitude = 1;
+
+    LOMAS_SERVICE_amqp__url = "amqp://${config.lomas.rabbitmq.host}:${toString config.lomas.rabbitmq.port}";
+    LOMAS_SERVICE_amqp__username = config.lomas.rabbitmq.user;
+    LOMAS_SERVICE_amqp__password = config.lomas.rabbitmq.password;
+    LOMAS_SERVICE_amqp__heartbeat = config.lomas.rabbitmq.heartbeat;
+    LOMAS_SERVICE_opendp_features = toPydanticSetting [
+      "contrib"
+      "floating-point"
+      "honest-but-curious"
+    ];
+    LOMAS_SERVICE_admin_database__url = config.lomas.mongo.dsn;
+    LOMAS_SERVICE_admin_database__username = config.lomas.mongo.user;
+    LOMAS_SERVICE_admin_database__password = config.lomas.mongo.password;
+    LOMAS_SERVICE_admin_database__max_pool_size = config.lomas.mongo.maxPoolSize;
+    LOMAS_SERVICE_admin_database__min_pool_size = config.lomas.mongo.minPoolSize;
+    LOMAS_SERVICE_admin_database__max_connecting = config.lomas.mongo.maxConnecting;
+    LOMAS_SERVICE_authenticator__authentication_type = "jwt";
+    LOMAS_SERVICE_authenticator__keycloak_url = "http://localhost:${toString config.lomas.keycloak.httpPort}";
+    LOMAS_SERVICE_authenticator__realm = config.lomas.realm;
+
+    LOMAS_SERVICE_telemetry__enabled = "false";
+    LOMAS_SERVICE_telemetry__service_name = "lomas-server-app";
+    LOMAS_SERVICE_telemetry__service_id = "default-host";
+    LOMAS_SERVICE_telemetry__collector_endpoint = "http://localhost:${toString config.lomas.telemetry.services.otlp.ports.grpc}";
+    LOMAS_SERVICE_telemetry__collector_insecure = "true";
+
+    # Lomas client environment
+    LOMAS_CLIENT_KEYCLOAK_URL = "http://${config.lomas.keycloak.host}:${toString config.lomas.keycloak.httpPort}";
+    LOMAS_CLIENT_REALM = config.lomas.realm;
+    LOMAS_CLIENT_APP_URL = "http://localhost:${toString config.lomas.port}";
+
+    LOMAS_CLIENT_telemetry__enabled = "false";
+    LOMAS_CLIENT_telemetry__service_name = "lomas-server-app";
+    LOMAS_CLIENT_telemetry__service_id = "default-host";
+    LOMAS_CLIENT_telemetry__collector_endpoint = "http://localhost:${toString config.lomas.telemetry.services.otlp.ports.grpc}";
+    LOMAS_CLIENT_telemetry__collector_insecure = "true";
+
+    # Keycloak setup
+    LOMAS_KC_SETUP_KEYCLOAK_URL = "http://${config.lomas.keycloak.host}:${toString config.lomas.keycloak.httpPort}";
+    LOMAS_KC_SETUP_KEYCLOAK_AUTHENTICATION_REALM = kc_auth_realm;
+    LOMAS_KC_SETUP_KEYCLOAK_ADMIN_CLIENT_ID = kc_admin_client_id;
+    LOMAS_KC_SETUP_KEYCLOAK_ADMIN_USER = config.lomas.keycloak.bootstrapAdminUser;
+    LOMAS_KC_SETUP_KEYCLOAK_ADMIN_PWD = config.lomas.keycloak.bootstrapAdminPass;
+    LOMAS_KC_SETUP_LOMAS_REALM = config.lomas.realm;
+    LOMAS_KC_SETUP_LOMAS_GATEWAY_URL = "http://example.com"; # TODO fix this
+    LOMAS_KC_SETUP_LOMAS_GATEWAY_CLIENT_ID = "lomas-oauth-proxy";
+    LOMAS_KC_SETUP_LOMAS_GATEWAY_CLIENT_SECRET = "lomas-oauth-proxy";
+    LOMAS_KC_SETUP_LOMAS_ADMIN_CLIENT_ID = config.lomas.admin.client_id;
+    LOMAS_KC_SETUP_LOMAS_ADMIN_CLIENT_SECRET = config.lomas.admin.client_secret;
+    LOMAS_KC_SETUP_LOMAS_API_CLIENT_ID = config.lomas.api.client_id;
+    LOMAS_KC_SETUP_LOMAS_API_CLIENT_SECRET = config.lomas.api.client_secret;
+    LOMAS_KC_SETUP_OVERWRITE_REALM = "true";
+
+    # Lomas demo setup
+    LOMAS_ADMIN_server_url = "http://localhost:${toString config.lomas.port}"; # public lomas service url from dashboard
+    LOMAS_ADMIN_server_service = "http://localhost:${toString config.lomas.port}";
+    LOMAS_ADMIN_MG_CONFIG__url = config.lomas.mongo.dsn;
+    LOMAS_ADMIN_MG_CONFIG__username = config.lomas.mongo.user;
+    LOMAS_ADMIN_MG_CONFIG__password = config.lomas.mongo.password;
+    LOMAS_ADMIN_KC_CONFIG__URL = "http://${config.lomas.keycloak.host}:${toString config.lomas.keycloak.httpPort}";
+    LOMAS_ADMIN_KC_CONFIG__REALM = config.lomas.realm;
+    LOMAS_ADMIN_KC_CONFIG__CLIENT_ID = config.lomas.admin.client_id;
+    LOMAS_ADMIN_KC_CONFIG__CLIENT_SECRET = config.lomas.admin.client_secret;
+    LOMAS_ADMIN_PATH_PREFIX = admin_path_prefix;
+    LOMAS_ADMIN_USER_YAML = user_yaml_path;
+    LOMAS_ADMIN_DATASET_YAML = dataset_yaml_path;
+  }
+  // (listToPydanticEnvVar "LOMAS_SERVICE_private_db_credentials" [
     {
-      GREET = "Lomas env";
-
-      # Ensure `coverage` uses our project config
-      COVERAGE_RCFILE = config.lomas.hooks.projectConfigFile;
-
-      # Pydantic note:
-      # Even when using a dotenv file, pydantic will still read environment variables as well as the dotenv file, environment variables will always take priority over values loaded from a dotenv file.
-
-      # Lomas Server Runtime
-      LOMAS_SERVICE_server__host_ip = config.lomas.host;
-      LOMAS_SERVICE_server__host_port = config.lomas.port;
-      LOMAS_SERVICE_server__log_level = "info";
-      LOMAS_SERVICE_server__reload = "true";
-      LOMAS_SERVICE_server__submit_limit = 300;
-      LOMAS_SERVICE_server__time_attack__method = "jitter";
-      LOMAS_SERVICE_server__time_attack__magnitude = 1;
-
-      LOMAS_SERVICE_amqp__url = "amqp://${config.lomas.rabbitmq.host}:${toString config.lomas.rabbitmq.port}";
-      LOMAS_SERVICE_amqp__username = config.lomas.rabbitmq.user;
-      LOMAS_SERVICE_amqp__password = config.lomas.rabbitmq.password;
-      LOMAS_SERVICE_amqp__heartbeat = config.lomas.rabbitmq.heartbeat;
-      LOMAS_SERVICE_opendp_features = toPydanticSetting [
-        "contrib"
-        "floating-point"
-        "honest-but-curious"
-      ];
-      LOMAS_SERVICE_admin_database__url = config.lomas.mongo.dsn;
-      LOMAS_SERVICE_admin_database__username = config.lomas.mongo.user;
-      LOMAS_SERVICE_admin_database__password = config.lomas.mongo.password;
-      LOMAS_SERVICE_admin_database__max_pool_size = config.lomas.mongo.maxPoolSize;
-      LOMAS_SERVICE_admin_database__min_pool_size = config.lomas.mongo.minPoolSize;
-      LOMAS_SERVICE_admin_database__max_connecting = config.lomas.mongo.maxConnecting;
-      LOMAS_SERVICE_authenticator__authentication_type = "jwt";
-      LOMAS_SERVICE_authenticator__keycloak_url = "http://localhost:${toString config.lomas.keycloak.httpPort}";
-      LOMAS_SERVICE_authenticator__realm = config.lomas.realm;
-
-      LOMAS_SERVICE_telemetry__enabled = "false";
-      LOMAS_SERVICE_telemetry__service_name = "lomas-server-app";
-      LOMAS_SERVICE_telemetry__service_id = "default-host";
-      LOMAS_SERVICE_telemetry__collector_endpoint = "http://localhost:${toString config.lomas.telemetry.services.otlp.ports.grpc}";
-      LOMAS_SERVICE_telemetry__collector_insecure = "true";
-
-      # Lomas client environment
-      LOMAS_CLIENT_KEYCLOAK_URL = "http://${config.lomas.keycloak.host}:${toString config.lomas.keycloak.httpPort}";
-      LOMAS_CLIENT_REALM = config.lomas.realm;
-      LOMAS_CLIENT_APP_URL = "http://localhost:${toString config.lomas.port}";
-
-      LOMAS_CLIENT_telemetry__enabled = "false";
-      LOMAS_CLIENT_telemetry__service_name = "lomas-server-app";
-      LOMAS_CLIENT_telemetry__service_id = "default-host";
-      LOMAS_CLIENT_telemetry__collector_endpoint = "http://localhost:${toString config.lomas.telemetry.services.otlp.ports.grpc}";
-      LOMAS_CLIENT_telemetry__collector_insecure = "true";
-
-      # Keycloak setup
-      LOMAS_KC_SETUP_KEYCLOAK_URL = "http://${config.lomas.keycloak.host}:${toString config.lomas.keycloak.httpPort}";
-      LOMAS_KC_SETUP_KEYCLOAK_AUTHENTICATION_REALM = kc_auth_realm;
-      LOMAS_KC_SETUP_KEYCLOAK_ADMIN_CLIENT_ID = kc_admin_client_id;
-      LOMAS_KC_SETUP_KEYCLOAK_ADMIN_USER = config.lomas.keycloak.bootstrapAdminUser;
-      LOMAS_KC_SETUP_KEYCLOAK_ADMIN_PWD = config.lomas.keycloak.bootstrapAdminPass;
-      LOMAS_KC_SETUP_LOMAS_REALM = config.lomas.realm;
-      LOMAS_KC_SETUP_LOMAS_GATEWAY_URL = "http://example.com"; # TODO fix this
-      LOMAS_KC_SETUP_LOMAS_GATEWAY_CLIENT_ID = "lomas-oauth-proxy";
-      LOMAS_KC_SETUP_LOMAS_GATEWAY_CLIENT_SECRET = "lomas-oauth-proxy";
-      LOMAS_KC_SETUP_LOMAS_ADMIN_CLIENT_ID = config.lomas.admin.client_id;
-      LOMAS_KC_SETUP_LOMAS_ADMIN_CLIENT_SECRET = config.lomas.admin.client_secret;
-      LOMAS_KC_SETUP_LOMAS_API_CLIENT_ID = config.lomas.api.client_id;
-      LOMAS_KC_SETUP_LOMAS_API_CLIENT_SECRET = config.lomas.api.client_secret;
-      LOMAS_KC_SETUP_OVERWRITE_REALM = "true";
-
-      # Lomas demo setup
-      LOMAS_ADMIN_server_url = "http://localhost:${toString config.lomas.port}"; # public lomas service url from dashboard
-      LOMAS_ADMIN_server_service = "http://localhost:${toString config.lomas.port}";
-      LOMAS_ADMIN_MG_CONFIG__url = config.lomas.mongo.dsn;
-      LOMAS_ADMIN_MG_CONFIG__username = config.lomas.mongo.user;
-      LOMAS_ADMIN_MG_CONFIG__password = config.lomas.mongo.password;
-      LOMAS_ADMIN_KC_CONFIG__URL = "http://${config.lomas.keycloak.host}:${toString config.lomas.keycloak.httpPort}";
-      LOMAS_ADMIN_KC_CONFIG__REALM = config.lomas.realm;
-      LOMAS_ADMIN_KC_CONFIG__CLIENT_ID = config.lomas.admin.client_id;
-      LOMAS_ADMIN_KC_CONFIG__CLIENT_SECRET = config.lomas.admin.client_secret;
-      LOMAS_ADMIN_PATH_PREFIX = admin_path_prefix;
-      LOMAS_ADMIN_USER_YAML = user_yaml_path;
-      LOMAS_ADMIN_DATASET_YAML = dataset_yaml_path;
+      credentials_name = "minio";
+      db_type = "S3_DB";
+      access_key_id = config.lomas.minio.rootUser;
+      secret_access_key = config.lomas.minio.rootPassword;
     }
-    // (listToPydanticEnvVar "LOMAS_SERVICE_private_db_credentials" [
-      {
-        credentials_name = "minio";
-        db_type = "S3_DB";
-        access_key_id = config.lomas.minio.rootUser;
-        secret_access_key = config.lomas.minio.rootPassword;
-      }
-    ])
-    // (listToPydanticEnvVar "LOMAS_KC_SETUP_LOMAS_ADMIN_USERS" [
-      {
-        username = "admin";
-        email = "admin@example.com";
-        temp_password = "admin";
-        first_name = "admin";
-        last_name = "ofAllAdmins";
-      }
-    ]);
+  ])
+  // (listToPydanticEnvVar "LOMAS_KC_SETUP_LOMAS_ADMIN_USERS" [
+    {
+      username = "admin";
+      email = "admin@example.com";
+      temp_password = "admin";
+      first_name = "admin";
+      last_name = "ofAllAdmins";
+    }
+  ]);
 
   cachix.pull = [ "lomas" ];
 
-  packages =
-    [
-      # required for up pip git+https in containers
-      pkgs.git
-      pkgs.cacert
-    ]
-    # Additional useful packages
-    ++ lib.optionals (!config.container.isBuilding) [
-      pkgs.jq
-      pkgs.yq-go
-      pkgs.watchexec
-      pkgs.kubectl
-      pkgs.kubernetes-helm
-    ];
+  packages = [
+    # required for up pip git+https in containers
+    pkgs.git
+    pkgs.cacert
+  ]
+  # Additional useful packages
+  ++ lib.optionals (!config.container.isBuilding) [
+    pkgs.jq
+    pkgs.yq-go
+    pkgs.watchexec
+    pkgs.kubectl
+    pkgs.kubernetes-helm
+  ];
 
   languages.nix.enable = !config.container.isBuilding;
 
@@ -385,13 +383,13 @@ in
             command = "pytest --cov-append --cov-report term-missing --cov --no-cov-on-fail --cov-config=${config.env.COVERAGE_RCFILE} \"$@\"";
             depends_on = {
               worker.condition = "process_started";
-              minio.condition = "process_ready";
-              mongodb.condition = "process_ready";
+              minio.condition = "process_healthy";
+              mongodb.condition = "process_healthy";
               mongodb-configure.condition = "process_completed_successfully";
-              keycloak.condition = "process_ready";
-              rabbitmq.condition = "process_ready";
+              keycloak.condition = "process_healthy";
+              rabbitmq.condition = "process_healthy";
               keycloak-setup.condition = "process_completed_successfully";
-              lomas-server.condition = "process_ready";
+              lomas-server.condition = "process_healthy";
             };
             log_location = "$DEVENV_ROOT/logs/pytest.log";
             log_configuration.flush_each_line = true;
@@ -549,6 +547,8 @@ in
 
     $b- Trick: seeing my prints whilst pytesting:$n
     pytest -s / pytest -rA
+
+    optionally --log-cli-level=DEBUG to get spammed again !
 
     $b- Just run the coverage alreaaady$n
     ut-coverage

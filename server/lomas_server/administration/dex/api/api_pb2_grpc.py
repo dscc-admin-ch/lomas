a7c40dbeafec322bce3d3f893a040be4e5b4993d
Bruno Studer <bruno.studer@protonmail.com>
2026-02-10 15:35:07 +0100

    [devenv] bump .lock (-> grpc 1.76)

--- server/lomas_server/administration/dex/api/api_pb2_grpc.py
+++ server/lomas_server/administration/dex/api/api_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from lomas_server.administration.dex.api import api_pb2 as lomas__server_dot_administration_dot_dex_dot_api_dot_api__pb2
 
-GRPC_GENERATED_VERSION = '1.75.0'
+GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
@@ -18,7 +18,7 @@ except ImportError:
 if _version_not_supported:
     raise RuntimeError(
         f'The grpc package installed is at version {GRPC_VERSION},'
-        + f' but the generated code in lomas_server/administration/dex/api/api_pb2_grpc.py depends on'
+        + ' but the generated code in lomas_server/administration/dex/api/api_pb2_grpc.py depends on'
         + f' grpcio>={GRPC_GENERATED_VERSION}.'
         + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
         + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'

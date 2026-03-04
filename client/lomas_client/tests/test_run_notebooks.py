54e113469ef4cbccc0cc1d8724d2690eb8e3106e
LancelotMarti <lancelot.marti@gmail.com>
2026-03-04 16:27:58 +0100

    Skip notebook with opendp legacy

--- client/lomas_client/tests/test_run_notebooks.py
+++ client/lomas_client/tests/test_run_notebooks.py
@@ -16,7 +16,7 @@ def mark_notebook(notebooks: list[Path]) -> list[NamedTuple]:
         "Demo_Client_Notebook_Smartnoise-Synth.ipynb": pytest.mark.skip(reason="Issue #423"),
         "Minimalist_Demo_Client_Notebook.ipynb": pytest.mark.long,  # ~8s
         "Penguin_Research.ipynb": pytest.mark.xfail(reason="How do you even VAR in ODP ???"),  # ~9s
-        "Queries Testing.ipynb": pytest.mark.long,  # ~49s
+        "Queries Testing.ipynb": pytest.mark.skip(reason="OpenDP legacy removed"),  # ~49s
     }
     return [pytest.param(file, marks=marks.get(file.name, [])) for file in notebooks]
 

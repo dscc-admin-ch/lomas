34945064e7c2d0940b769dd6dace506581937913
Damien <damien.aymon@bfs.admin.ch>
2026-07-28 08:25:59 +0200

    cleanup

--- core/lomas_core/models/constants.py
+++ core/lomas_core/models/constants.py
@@ -93,8 +93,7 @@ def init_logging(name: str, level: str = "INFO", lomas_level: str = "INFO") -> N
     console = Console(width=200, force_terminal=True)
     logging.basicConfig(
         format="%(asctime)s - %(message)s - %(name)s",
-        # datefmt="[%X]",
-        datefmt="[%H:%M:%S.%f]",
+        datefmt="[%H:%M:%S]",
         handlers=[
             RichHandler(console=console, show_time=True, rich_tracebacks=False, tracebacks_show_locals=True)
         ],
--- server/lomas_server/administration/scripts/lomas_demo_setup.py
+++ server/lomas_server/administration/scripts/lomas_demo_setup.py
@@ -61,8 +61,6 @@ def add_lomas_demo_data(config: DemoAdminConfig) -> IOResultE:
         headers={"Authorization": f"Bearer {config.bootstrap}"},
     )
 
-    pprint(add_users)
-
     add_dex_users: IOResultE = flow(
         config.dex_config,  # DexAdminConfig | None
         Maybe.from_optional,  # Maybe[DexAdminConfig]
